23399c3003372bb3443079769c529c373464617f
MuflonnLukavson <koteklukas55@seznam.cz>
2025-05-09 10:33:02 +0200

    testing 0905

--- vypocet.py
+++ vypocet.py
@@ -43,7 +43,8 @@ def kick_pos(ball,post1,post2, me): # najdi místo výkopu
     kick = [new_ball[0]+ball_mid_vec[0]/ratio, new_ball[1]+ball_mid_vec[1]/ratio]
     det_pos = [new_ball[0]-ball_mid_vec[0]/ratio, new_ball[1]+ball_mid_vec[1]/ratio] # kdyby bylo třeba objížďka
 
-
+    me2ball = [new_ball[0] - me[0], new_ball[1] - me[1]]
+    print("me2ball", me2ball)
     det_pos2= [new_ball[0] - ball_mid_vec[0] / ratio, new_ball[1]]
     print(det_pos, det_pos2)
     return kick, det_pos
