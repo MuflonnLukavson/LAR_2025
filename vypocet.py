453a39306cc3a91fe49a6a43d30685bfc10b23ca
MuflonnLukavson <koteklukas55@seznam.cz>
2025-05-12 09:45:45 +0200

    changed kick_off_dist to 0,7

--- vypocet.py
+++ vypocet.py
@@ -3,7 +3,7 @@ import tube_seg as seg
 import objects as obj
 import numpy as np
 
-kick_off_dist = 0.6 # jak daleko od míče chceme vykopávat
+kick_off_dist = 0.7 # jak daleko od míče chceme vykopávat
 nuh_uh = 3 # believable distance
 
 def already_seen(curr_objects, new_obj): # takes array of objects and compares their position to a new object to evaluate if it is actually a new one
